2be824c625c48ae5ccad5c43356dc3de2e1455f0
itggot-john-viberud <john.viberud@itggot.se>
2018-05-18 14:03:46 +0200

    meny och fixat bilder för mobil

--- slutprojekt/site/js/menu.js
+++ slutprojekt/site/js/menu.js
@@ -1,6 +1,13 @@
 function toggleMenu(){
-    var element = document.querySelector(".menu");
+    console.log("omg")
+    var element = document.querySelector(".menu_mobil");
     element.classList.toggle("show");
+    var element = document.querySelector(".menu_mobil_fixed");
+    element.classList.toggle("open");
+    if(element.classList.contains("open"))
+        {element.innerHTML = "close"}
+        else
+        {element.innerHTML = "menu"}
 
     // element = document.querySelector("main")
 }
